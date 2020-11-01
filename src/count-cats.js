77acbe33adef701172b109b82fe7f37ab9e43a08
Veligan Vladlena <vladleshka99@mail.ru>
2020-11-01 22:20:21 +0300

    init: add solution what-season and count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(arr) {
+    let sum = 0;
+    let newArr = arr.flat();
+
+    newArr.map(function(elem) {
+        if (elem === '^^') return sum++
+            else {
+                return sum
+            }
+
+    });
+    return sum;
+}
